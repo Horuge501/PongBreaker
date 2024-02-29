44f2aa98d8bcda96a1e951253751f651ddbfa0fe
Horuge501 <horuge501@gmail.com>
2024-02-28 23:34:41 -0600

    Added Menus

---
+++ Assets/Scripts/PanelScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelScript : MonoBehaviour
+{
+    [SerializeField] private GameObject playerScore;
+    [SerializeField] private GameObject computerScore;
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void ClosePanel()
+    {
+        GetComponent<Animator>().SetTrigger("Close");
+
+    }
+
+    public void SetTimePlay()
+    {
+        Time.timeScale = 1;
+        playerScore.SetActive(true);
+        computerScore.SetActive(true);
+    }
+}
